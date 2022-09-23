9bbce76e9d8cee113d668b00b326e29b05791b4a
Hayrullah DEDELER <h.dede515151@gmail.com>
2022-09-24 00:08:46 +0300

    011

--- src/main/java/C01.java
+++ src/main/java/C01.java
@@ -1,5 +1,8 @@
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Hayrullah");
+        System.out.println("Hayrullah");
+        System.out.println("Hayrullah");
+
     }
 }
